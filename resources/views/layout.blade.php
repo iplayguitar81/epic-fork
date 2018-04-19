19fa0415fba67b9a4e78452e1479c81700473a49
Chris M Snyder <iplayguitar81@yahoo.com>
2018-04-18 22:19:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1064,7 +1064,7 @@ em.caption-em{
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand triad-lg-brand" href="{{url('/')}}">
-        <img id="brand-image" src="{{url('images/shareyumlogo-start.png')}}" alt="ShareYum.com Logo">
+        <img id="brand-image" src="{{url('images/epicforkdraft.png')}}" alt="EpicFork.com Logo">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
