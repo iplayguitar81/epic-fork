e769e328cf05d38ae8cb96bd543750071cee2c41
Chris M Snyder <iplayguitar81@yahoo.com>
2018-04-30 21:29:47 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,9 @@
 <div class="panel-footer">
+    <br/>
+    <br/>
     EpicFork.com &copy; {{date("Y")}}
     <br/>
     <a class="text-center" href="{{ route('disclaimer') }}">Site Disclaimer</a>
+    <br/>
 </div>
 
