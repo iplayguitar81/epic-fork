364ab0995cef3feadc0b3a9ec64b2b3b9a270ea7
Chris M Snyder <iplayguitar81@yahoo.com>
2018-05-08 21:13:05 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'Map Laravel Test')
+@section('title', 'Search Results')
 
 
 @section('content')
@@ -21,41 +21,54 @@
 
     @foreach($results2 as $result)
 
-            <div class="row justify-content-md-center">
-                <div class="col-sm-5 text-center">
 
-                    @if( $result->videoPath !=null)
-                        <div class="video-container">
-                            {!! $result->videoPath !!}
-                        </div>
-                    @else
-                        <img src="../images/md-img-{{ $result->imgPath}}" class="img-responsive">
-                    @endif
-                    <br/>
-                    <span>{{$result->mainImg_caption}}</span>
-                </div>
-                <div class="col-sm-7 text-center">
+        <div class="col-lg-5 offset-lg-1 col-md-11 col-md-11-offset-1 col-sm-11 col-sm-offset-1 col-11 d-block mx-auto">
+            <br/>
+            <a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}"><img src="../images/md-img-{{ $result->imgPath}}" class="img-fluid mx-auto"></a>
+            <br/>
+            <br/>
+            <span class="text-muted"> {{strtoupper (gameDate($result->created_at))}}</span>
+            <br/>
+            <span class="text-muted">Written by <a href="#">{{written_by($result->user_id)}}</a></span>
+            <h2 class="chalkboard"><a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">{{ ucwords($result->title)}}</a></h2>
 
-                    <h2 class="secondary-posts-title"><a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">{{ ucwords($result->title)}}</a>
-                       <a href="{{ route_articles($result->category)}}">
-                        <button type="button" class="btn btn-default btn-sm">
-                            <span class="glyphicon glyphicon-folder-open"></span>
-                            {{$result->category}}
-                        </button>
-                       </a>
+        </div>
 
-                    </h2>
-                    <p class="text-muted">{{$result->subHead}}</p>
-                    <p class="text-muted">Written by <a href="#">{{written_by($result->user_id)}}</a> on {{gameDate($result->created_at)}} </p>
+            {{--<div class="row justify-content-md-center">--}}
+                {{--<div class="col-sm-5 text-center">--}}
+
+                    {{--@if( $result->videoPath !=null)--}}
+                        {{--<div class="video-container">--}}
+                            {{--{!! $result->videoPath !!}--}}
+                        {{--</div>--}}
+                    {{--@else--}}
+                        {{--<img src="../images/md-img-{{ $result->imgPath}}" class="img-responsive">--}}
+                    {{--@endif--}}
+                    {{--<br/>--}}
+                    {{--<span>{{$result->mainImg_caption}}</span>--}}
+                {{--</div>--}}
+                {{--<div class="col-sm-7 text-center">--}}
+
+                    {{--<h2 class="secondary-posts-title"><a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">{{ ucwords($result->title)}}</a>--}}
+                       {{--<a href="{{ route_articles($result->category)}}">--}}
+                        {{--<button type="button" class="btn btn-default btn-sm">--}}
+                            {{--<span class="glyphicon glyphicon-folder-open"></span>--}}
+                            {{--{{$result->category}}--}}
+                        {{--</button>--}}
+                       {{--</a>--}}
+
+                    {{--</h2>--}}
+                    {{--<p class="text-muted">{{$result->subHead}}</p>--}}
+                    {{--<p class="text-muted">Written by <a href="#">{{written_by($result->user_id)}}</a> on {{gameDate($result->created_at)}} </p>--}}
 
 
-                    <p>{{snippet($result->body)}}...<a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}"> <button type="button" class="btn btn-success btn-xs">READ MORE</button></a></p>
+                    {{--<p>{{snippet($result->body)}}...<a class="" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}"> <button type="button" class="btn btn-success btn-xs">READ MORE</button></a></p>--}}
 
 
-                </div>
-                <br/>
+                {{--</div>--}}
+                {{--<br/>--}}
 
-            </div>
+            {{--</div>--}}
             <br/>
             <hr/>
 
